5a80429f19a088a722677718107ab6ac88c18cb7
nithish-jadala <mailnithish2004@gmail.com>
2025-03-12 20:59:00 +0530

    Add files via upload

--- LAB2/function.dart
+++ LAB2/function.dart
@@ -1,9 +1,19 @@
 import 'dart:io';
-int sum(int a,int b)=>(a+b);
-int sub(int a,int b)=>(a-b);
-int div(int a,int b)=>(a~/b);
-int mul(int a,int b)=>(a*b);
-int mod(int a,int b)=>(a%b);
+int sum(int a,int b){
+return(a+b);
+}
+int sub(int a,int b){
+return(a-b);
+}
+int div(int a,int b){
+return(a~/b);
+}
+int mul(int a,int b){
+return(a*b);
+}
+int mod(int a,int b){
+return(a%b);
+}
 void main() {
 print("Enter 2 values:");
 int a = int.parse(stdin.readLineSync()!);
