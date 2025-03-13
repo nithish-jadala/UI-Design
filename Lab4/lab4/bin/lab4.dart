c8390b86fc8699c9dad150b3ead14ef02ac6fd6a
nithish-jadala <mailnithish2004@gmail.com>
2025-03-13 12:51:02 +0530

    Add files via upload

---
+++ Lab4/lab4/bin/lab4.dart
@@ -0,0 +1,10 @@
+import 'package:lab4/switch2.dart';
+import 'dart:io';
+void main(List<String> arguments) {
+switch2 sd=switch2();
+	print("enter month");
+	var x=stdin.readLineSync()!;
+	sd.demo(x);
+}
+
+  
