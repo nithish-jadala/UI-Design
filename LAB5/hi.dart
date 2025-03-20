a8ce665b20a1f4e35ec25a3e55f672fc38622cf7
nithish-jadala <mailnithish2004@gmail.com>
2025-03-20 12:43:16 +0530

    Update hi.dart

--- LAB5/hi.dart
+++ LAB5/hi.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const Lapp());
+}
+class Lapp extends StatelessWidget {
+  const Lapp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(color: Colors.blue, child: Text('Nithin')),
+              Container(color: Colors.red, child: Text('Nithin')),
+              Container(color: Colors.orange, child: Text('Nithin')),
+              Container(color: Colors.brown, child: Text('Nithin')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
